421b41048045d8df6f45819142f01503da534566
Roger King <rking@zillion.com>
2017-10-12 01:09:38 -0400

    using view_config:

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
-from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
-from pyramid.response import Response
-
-def hello_world(request):
-    print('Incoming request')
-    return Response('<body> <h1>Hello World </h1></body>')
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.add_route('hello','/')
-    config.add_view(hello_world, route_name='hello')
+    config.add_route('home','/')
+    config.add_route('hello', '/howdy')
+    config.scan('.views')
     return config.make_wsgi_app()
